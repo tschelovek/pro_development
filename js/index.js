dc36ff35213235bee4746f80f1b45e445a6aa91d
tschelovek <az18rg@gmail.com>
2023-08-22 14:11:51 +0300

    gp in work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// import {el} from './libs/redom.es'
+
 document.addEventListener('DOMContentLoaded', () => {
 
     Fancybox.bind('[data-fancybox]', {
@@ -34,21 +36,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const swiper2 = new Swiper('#slider_gallery_1', {
         loop: true,
-        // autoplay: {
-        //     delay: 3000,
-        // },
+        autoplay: {
+            delay: 3000,
+        },
         navigation: {
-            nextEl: '#btn_gallery_next',
-            prevEl: '#btn_gallery_prev',
+            nextEl: '#btn_gallery_1_next',
+            prevEl: '#btn_gallery_1_prev',
         },
         speed: 1000,
         slidesPerView: 1,
     })
 
-    const swiper3 = new Swiper('#slider_projects', {
+    //* Инициализируем второй слайдер в Галерее и сразу его дисейблим
+    const swiper3 = new Swiper('#slider_gallery_2', {
         loop: true,
         autoplay: {
-            delay: 3000,
+            delay: 2000,
+        },
+        navigation: {
+            nextEl: '#btn_gallery_2_next',
+            prevEl: '#btn_gallery_2_prev',
+        },
+        speed: 1000,
+        slidesPerView: 1,
+    }).disable()
+
+    const swiper4 = new Swiper('#slider_projects', {
+        loop: true,
+        autoplay: {
+            delay: 2000,
         },
         speed: 1000,
         // pagination: {
@@ -77,6 +93,34 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    /**
+     * Переключение слайдеров в разделе "Галерея"
+     */
+    const tabButtons = document.querySelectorAll('section.gallery button[data-tab-index]');
+
+    tabButtons.forEach(tabBtn => tabBtn.addEventListener('click', e => {
+        const target = e.currentTarget;
+
+        if (target.classList.contains('active')) return;
+
+        tabButtons.forEach(btn => btn.classList.remove('active'));
+        target.classList.add('active');
+
+        document.querySelectorAll('section.gallery .swiper')
+            .forEach(slider => {
+                    if(slider.dataset.target === target.dataset.tabIndex) {
+                        slider.style.display = 'block'
+                        slider.swiper.enable();
+                        slider.swiper.update();
+                        slider.swiper.autoplay.resume();
+                    } else {
+                        slider.swiper.disable();
+                        slider.style.display = 'none'
+                    }
+                }
+            )
+    }))
+
     // Fancybox.show([{ src: "#dialog-content", type: "inline" }]);
 
     const modalView = document.getElementById('modal_view');
@@ -94,36 +138,63 @@ document.addEventListener('DOMContentLoaded', () => {
         const projectsSpecsClone = item.querySelector('.projects__specs').cloneNode(true);
         const projectsSpecsNest = modalView.querySelector('.modal_view__block_right__specs-nest');
 
-        projectsSpecsNest.append(projectsSpecsClone);
+        // projectsSpecsNest.append(projectsSpecsClone);
 
         if (modalView.classList.contains('open')) {
             closeModalView()
             return
         }
 
-        // modalView.innerHTML = `
-        //     <div class="modal_view__wrapper">
-        //         <div class="container">
-        //             <div class="modal_view__block_left">
-        //                 <div class="modal_view__block_left__controls">
-        //                     <button class="btn" type="button" id="close_modal_view">close</button>
-        //                     <a href="/" class="logo__link">
-        //                         <img srcset="./images/logo.png, ./images/logo_x2.png x2"
-        //                              src="./images/logo_x2.png"
-        //                              alt=""
-        //                              title=""
-        //                         >
-        //                     </a>
-        //                 </div>
+        const layout = el('container',
+            el('modal_view__wrapper', [
+                    el('modal_view__block_left',
+                        el('modal_view__block_left__controls'), [
+                            el('button.btn', {type: "button", id: "close_modal_view"}, "Close"),
+                            el('a.logo__link', {href: "/"},
+                                el('', {src: "./images/logo_x2.png"}
+                                )
+                            )
+                        ]
+                    ),
+                    el('modal_view__block_right', [
+                            el('modal_view__block_right__controls'),
+                            el('modal_view__block_right__info',
+                                el('modal_view__block_right__specs-nest',
+                                    // projectsSpecsClone
+                                )
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+
+
+        // <div className="container">
+        //     <div className="modal_view__wrapper">
+        //         <div className="modal_view__block_left">
+        //             <div className="modal_view__block_left__controls">
+        //                 <button className="btn" type="button" id="close_modal_view">close</button>
+        //                 <a href="/" className="logo__link">
+        //                     <img srcSet="./images/logo.png, ./images/logo_x2.png x2"
+        //                          src="./images/logo_x2.png"
+        //                          alt=""
+        //                          title=""
+        //                     >
+        //                 </a>
         //             </div>
-        //             <div class="modal_view__block_right">
-        //                 <div class="modal_view__block_right__top"></div>
-        //                 <div class="modal_view__info">
-        //                 </div>
+        //         </div>
+        //         <div className="modal_view__block_right">
+        //             <div className="modal_view__block_right__controls">
+        //
+        //             </div>
+        //             <div className="modal_view__block_right__info">
+        //                 <div className="modal_view__block_right__specs-nest"></div>
         //             </div>
         //         </div>
         //     </div>
-        // `;
+        // </div>
+
         modalView.classList.add('open')
     }
 
@@ -134,7 +205,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     /**
      *
-     * Бургер меню8`
+     * Бургер меню
      */
     document.getElementById('burgerButton')?.addEventListener('click', e => {
         e.preventDefault();
